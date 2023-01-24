e0b9604f393bb3d0e72ff1eec38aebfc8b64eee0
unknown <coffecraft38@gmail.com>
2023-01-24 22:19:06 +0200

    version 0.01

--- TSOTSS.py
+++ TSOTSS.py
@@ -1,5 +1,5 @@
 
-import planets as pls
+
 import pygame
 import math
 pygame.init()
@@ -24,11 +24,6 @@ GRAY = (58, 54, 60)
 
 FONT = pygame.font.SysFont("comicsans", 16)
 
-time_speed = 1
-
-win_text = FONT.render((1 * time_speed) + "x")
-pos = win_text.get_rect(20, 10)
-
 #theta = math.atan2(distance_y, distance_x)#
 
 
@@ -46,10 +41,6 @@ class Planet:
     SCALE = 150 / AU
 
     Decrease = 1
-
-    direction_y = 0
-
-    direction_x = 0
 
     Lines = True
 
@@ -127,6 +118,34 @@ def main():
     run = True
     clock = pygame.time.Clock()
 
+    sun = Planet(0, 0, 30, YELLOW, 1.98892*10**30)
+    sun.sun = True
+    
+    earth = Planet(-1 * Planet.AU, 0 , 16, BLUE,  5.9736*10**24)
+    earth.y_vel = 29.783 * 1000
+    
+    mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10**23)
+    mars.y_vel = 24.077 * 1000
+    
+    mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GREY, 3.30 * 10**23)
+    mercury.y_vel = -47.4 * 1000
+    
+    venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10**24)
+    venus.y_vel = -35.02 * 1000
+    
+    jupyter = Planet(-5.2 * Planet.AU, 0, 20, BROWN, 1.8986*10**27)
+    jupyter.y_vel = -13.07 * 1000
+    
+    saturn = Planet(9.54 * Planet.AU, 0, 12, ORANGE, 58.232*10**6)
+    saturn.y_vel = -9.690 * 1000
+    
+    uranus = Planet(19.19 * Planet.AU,0, 16, LIGHT_BLUE,  25.362*10**6)
+    uranus.y_vel = 6.81 * 1000
+    
+    neptune = Planet(30.06 * Planet.AU, 0, 14, DEEP_BLUE, 24.622*10**6)
+    neptune.y_vel = 5.4349 * 1000
+    
+    planets = [sun, earth, mars, mercury, venus, jupyter, saturn, uranus, neptune]
 
     while run:
         clock.tick(60)
@@ -166,8 +185,8 @@ def main():
         
         
 
-        for planet in pls.planets:
-            planet.update_position(pls.planets)
+        for planet in planets:
+            planet.update_position(planets)
             planet.draw(WIN)
 
         
