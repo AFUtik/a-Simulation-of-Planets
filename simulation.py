239deaebfce4234d0cbaa366a8dc42c768fc9ded
AFUtik <coffecraft38@gmail.com>
2023-03-01 10:27:48 +0200

    decreased lines of the code

--- simulation.py
+++ simulation.py
@@ -9,17 +9,6 @@ WIDTH, HEIGHT =  1980, 1080
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Planet Simulation")
 
-WHITE = (255, 255, 255)
-YELLOW = (255, 255, 0)
-BLUE = (100, 149, 237)
-RED = (188, 39, 50)
-DARK_GREY = (80, 78, 81)
-BROWN = (106, 60, 31)
-ORANGE = (218, 79, 0)
-LIGHT_BLUE = (62, 142, 182)
-DEEP_BLUE = (62, 63, 190)
-GRAY = (58, 54, 60)
-
 class Planet:
     G = 6.67428e-11
     AU = 149.6e6 * 1000
@@ -45,7 +34,7 @@ class Planet:
         x = (self.x * self.SCALE + WIDTH / 2) + self.MOVE[0] * self.DECREASE
         y = (self.y * self.SCALE + HEIGHT / 2) + self.MOVE[1] * self.DECREASE
 
-        if len(self.orbit) > 2 and self.LINES == True: 
+        if len(self.orbit) > 2 and self.LINES: 
             updated_points = []
             for point in self.orbit:
                 x, y = point
@@ -55,9 +44,8 @@ class Planet:
                 updated_points.append((x, y))  
 
             if len(updated_points) > self.YOUR_LIMIT:
-                del updated_points[0:self.YOUR_LIMIT - 2]
-                self.orbit.clear()
-                
+                updated_points.clear()
+                self.orbit.clear() 
             else:
                 pygame.draw.lines(win, self.color, False, updated_points, 1)
             
@@ -93,31 +81,31 @@ class Planet:
         self.orbit.append((self.x, self.y))
 
 def main():
-    sun = Planet(0, 0, 30, YELLOW, 1.98892*10**30)
+    sun = Planet(0, 0, 30, (255, 255, 0) , 1.98892*10**30)
     sun.sun = True
     
-    earth = Planet(-1 * Planet.AU, 0 , 16, BLUE,  5.9736*10**24)
+    earth = Planet(-1 * Planet.AU, 0 , 16, (100, 149, 237) ,  5.9736*10**24)
     earth.y_vel = 29.783 * 1000
     
-    mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10**23)
+    mars = Planet(-1.524 * Planet.AU, 0, 12, (188, 39, 50) , 6.39 * 10**23)
     mars.y_vel = 24.077 * 1000
     
-    mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GREY, 3.30 * 10**23)
+    mercury = Planet(0.387 * Planet.AU, 0, 8, (80, 78, 81), 3.30 * 10**23)
     mercury.y_vel = -47.4 * 1000
     
-    venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10**24)
+    venus = Planet(0.723 * Planet.AU, 0, 14, (255, 255, 255) , 4.8685 * 10**24)
     venus.y_vel = -35.02 * 1000
     
-    jupyter = Planet(-5.2 * Planet.AU, 0, 20, BROWN, 1.8986*10**27)
+    jupyter = Planet(-5.2 * Planet.AU, 0, 20, (106, 60, 31), 1.8986*10**27)
     jupyter.y_vel = -13.07 * 1000
     
-    saturn = Planet(9.54 * Planet.AU, 0, 12, ORANGE, 58.232*10**6)
+    saturn = Planet(9.54 * Planet.AU, 0, 12, (218, 79, 0), 58.232*10**6)
     saturn.y_vel = -9.690 * 1000
     
-    uranus = Planet(19.19 * Planet.AU,0, 16, LIGHT_BLUE,  25.362*10**6)
+    uranus = Planet(19.19 * Planet.AU,0, 16, (62, 142, 182),  25.362*10**6)
     uranus.y_vel = 6.81 * 1000
     
-    neptune = Planet(30.06 * Planet.AU, 0, 14, DEEP_BLUE, 24.622*10**6)
+    neptune = Planet(30.06 * Planet.AU, 0, 14, (62, 63, 190), 24.622*10**6)
     neptune.y_vel = 5.4349 * 1000
     
     planets = [sun, earth, mars, mercury, venus, jupyter, saturn, uranus, neptune]
@@ -157,8 +145,8 @@ def main():
                     mass = 8 * S ** 2
 
             elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
-                x = -(WIDTH / 2 - (sp[0] - Planet.MOVE[0] * Planet.DECREASE)) / Planet.SCALE
-                y = -(HEIGHT / 2 - (sp[1] - Planet.MOVE[1] * Planet.DECREASE)) / Planet.SCALE
+                x = (-WIDTH / 2 + (sp[0] - Planet.MOVE[0] * Planet.DECREASE)) / Planet.SCALE
+                y = (-HEIGHT / 2 + (sp[1] - Planet.MOVE[1] * Planet.DECREASE)) / Planet.SCALE
 
                 your_planet = Planet(x, y, width / 2, color, mass)
 
